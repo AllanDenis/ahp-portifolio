35ae5e0ea2441259372a7d2960a7218b72046bc0
Allan Denis <allancomll@gmail.com>
2017-03-02 22:45:59 -0300

    Matrizes de comparação normalizadas funcionando. Tentando ler critérios.

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 import numpy as np
+from collections import OrderedDict
 from pprint import pprint
+from itertools import combinations
+from fractions import Fraction
+
+def matrizComp(itens):
+    numItens = len(itens)
+    itensComp = np.ones((numItens, numItens))
+    pares = combinations(range(numItens), 2)
+    for p1,p2 in pares:
+        entrada = input("Relação entre %s e %s: " % (itens[p1], itens[p2]))
+        itensComp[p1][p2] = Fraction(entrada)
+
+    # Preenchimento da matriz triangular
+    itensComp = np.triu(itensComp, k=1) +\
+                np.triu(np.reciprocal(itensComp)).T
+    # Normalização das colunas
+    itensComp /= itensComp.sum(axis=0)
+    return itensComp
+
+def vetorPrioridade(matriz):
+    return matriz.mean(axis=1)
 
 objetivo = ''
 criterios = []
@@ -7,13 +28,28 @@ alternativas = []
 criteriosComp = []
 alternativasComp = []
 
+# Critérios
 entrada = input("Digite os critérios, separados por espaços: ")
 criterios = entrada.split()
-criteriosComp = np.zeros((len(criterios), len(criterios)))
+criteriosComp = matrizComp(criterios)
+
+# Vetor de prioridade
+prioridades = vetorPrioridade(criteriosComp)
+
+for criterio,peso in zip(criterios, prioridades):
+    print("Peso do critério %s: %d%%" % (criterio, int(peso * 100)))
 
 
+# Alternativas
 entrada = input("Digite as alternativas, separadas por espaços: ")
-alternativas = entrada.split()
-alternativasComp = np.zeros((len(alternativas), len(alternativas)))
-pprint(criteriosComp)
-pprint(alternativasComp)
+alternativas = enumerate(entrada.split())
+alternativas = [(b,a) for a,b in alternativas]
+alternativas = OrderedDict(alternativas)
+for alt in alternativas:
+    pprint(alt)
+    for crit in criterios:
+         alternativas[alt][crit] = input("\n%s quanto ao critério %s:" %\
+                                        (alt, crit))
+    # alternativasComp.append(matrizComp(alternativas))
+
+pprint(alternativas)
