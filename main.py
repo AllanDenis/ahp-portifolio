18e1f447d05295548ec88fe1a504c6ea43ee4815
Allan Denis <allancomll@gmail.com>
2017-03-03 21:43:19 -0300

    Funcionando, porém sem validação de entradas.

--- main.py
+++ main.py
@@ -1,55 +1,49 @@
 import numpy as np
-from collections import OrderedDict
-from pprint import pprint
-from itertools import combinations
-from fractions import Fraction
-
-def matrizComp(itens):
-    numItens = len(itens)
-    itensComp = np.ones((numItens, numItens))
-    pares = combinations(range(numItens), 2)
-    for p1,p2 in pares:
-        entrada = input("Relação entre %s e %s: " % (itens[p1], itens[p2]))
-        itensComp[p1][p2] = Fraction(entrada)
-
-    # Preenchimento da matriz triangular
-    itensComp = np.triu(itensComp, k=1) +\
-                np.triu(np.reciprocal(itensComp)).T
-    # Normalização das colunas
-    itensComp /= itensComp.sum(axis=0)
-    return itensComp
-
-def vetorPrioridade(matriz):
-    return matriz.mean(axis=1)
 
 objetivo = ''
 criterios = []
 alternativas = []
-criteriosComp = []
-alternativasComp = []
+
+objetivo = input("Decisão a ser tomada: ")
 
 # Critérios
-entrada = input("Digite os critérios, separados por espaços: ")
-criterios = entrada.split()
-criteriosComp = matrizComp(criterios)
-
-# Vetor de prioridade
-prioridades = vetorPrioridade(criteriosComp)
-
-for criterio,peso in zip(criterios, prioridades):
-    print("Peso do critério %s: %d%%" % (criterio, int(peso * 100)))
-
+entrada = input("Critérios considerados, separados por espaços: ").split()
+for crit in entrada:
+    criterios.append({
+        'nome': crit,
+        'peso': float(input("Peso do critério %s na decisão: " % crit))
+    })
 
 # Alternativas
-entrada = input("Digite as alternativas, separadas por espaços: ")
-alternativas = enumerate(entrada.split())
-alternativas = [(b,a) for a,b in alternativas]
-alternativas = OrderedDict(alternativas)
-for alt in alternativas:
-    pprint(alt)
-    for crit in criterios:
-         alternativas[alt][crit] = input("\n%s quanto ao critério %s:" %\
-                                        (alt, crit))
-    # alternativasComp.append(matrizComp(alternativas))
+entrada = input("Alternativas disponíveis, separadas por espaços: ").split()
+for alt in entrada:
+    alternativas.append({
+        'nome': alt,
+        'pesos': [
+            float(input("%s de %s: " % (crit['nome'], alt)))
+            for crit in criterios
+        ]
+    })
 
-pprint(alternativas)
+# Matriz alternativa x critério com colunas normalizadas
+matrizAltCrit = [alt['pesos'] for alt in alternativas]
+matrizAltCrit = np.array(matrizAltCrit).T
+for coluna in matrizAltCrit:
+    coluna /= sum(coluna)
+matrizAltCrit = matrizAltCrit.T
+
+# Critérios normalizadas
+rankingCriterios = [crit['peso'] for crit in criterios]
+rankingCriterios = np.array(rankingCriterios)
+rankingCriterios /= sum(rankingCriterios)
+
+# RESULTADO: ranking de alternativas
+rankingAlternativas = matrizAltCrit.dot(rankingCriterios)
+rankingAlternativas = zip([a['nome'] for a in alternativas], rankingCriterios)
+rankingAlternativas = sorted([(pontos,alt)
+                        for (alt,pontos) in rankingAlternativas],
+                        reverse=True)
+
+print("Ranking de alternativas para %s: " % objetivo)
+for pontos, alt in rankingAlternativas:
+    print("%s: %.1f%%" % (alt, pontos * 100))
